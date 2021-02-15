4a9194120479ae747267b6fae58e2e315c7d322f
Ransom Bowman <ransombowman@ymail.com>
2021-02-14 22:52:02 -0700

    Initial Commit

---
+++ Homework2Main.py
@@ -0,0 +1,25 @@
+##import cv2 as cv
+##import numpy as np
+##
+##src = cv.imread("japaneseflowers.jpg", 1)
+##if src is None:
+##    print("missing image")
+##
+##cv.imshow("windoMy", src)
+### else:
+###     gray = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
+###     final = cv.Canny(gray, 75,200)
+###     cv.imshow("Original", gray)
+###     cv.imshow("adaptive", final)
+
+import cv2 as cv
+import numpy as np
+
+src = cv.imread("japaneseflowers.jpg", 1)
+if src is None:
+    print("missing image")
+else:
+    gray = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
+    final = cv.Canny(gray, 75,200)
+    cv.imshow("Original", gray)
+    cv.imshow("adaptive", final)   
